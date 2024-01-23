fe3f3f4de8a0be80e3a45e16e46cd535e4a2b4ef
Elena Pedko <elpg.ods@gmail.com>
2024-01-23 16:50:33 +0200

    task 4 is completed

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-// const key = new Key();
+class Key {
+    private signature: number;
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+    constructor() {
+        this.signature = Math.random();
+    }
 
-// house.openDoor(person.getKey());
+    getSignature(): number {
+        return this.signature;
+    }
+}
 
-// house.comeIn(person);
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    public door: boolean = false;
+    public tenants: Person[] = [];
+
+    constructor(public key: Key) {}
+
+    comeIn(person: Person): void {
+        if (this.door) {
+          this.tenants.push(person);
+          console.log('Door is open');
+        } else {
+          console.log('Door is closed');
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('Door is open');
+      } else {
+        console.log('Wrong key');
+      }
+    }
+  }
+
+const key = new Key();
+
+const house = new MyHouse(key);
+
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
 
 
-// export {};
+export {};
